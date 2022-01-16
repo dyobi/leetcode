c8b7a76cf7e70b17aec34cd5487caffbff4954bd
Dyobi <dyobi.92@gmail.com>
2022-01-16 22:46:52 +0900

    .

---
+++ problems/0414.py
@@ -0,0 +1,28 @@
+"414. Third Maximum Number"
+
+# Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+
+# Input: nums = [3,2,1] / Output: 1
+# Explanation:
+# The first distinct maximum is 3.
+# The second distinct maximum is 2.
+# The third distinct maximum is 1.
+
+# Input: nums = [1,2] / Output: 2
+# Explanation:
+# The first distinct maximum is 2.
+# The second distinct maximum is 1.
+# The third distinct maximum does not exist, so the maximum (2) is returned instead.
+
+# Input: nums = [2,2,3,1] / Output: 1
+# Explanation:
+# The first distinct maximum is 3.
+# The second distinct maximum is 2 (both 2's are counted together since they have the same value).
+# The third distinct maximum is 1.
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+
+        res = sorted(list(set(nums)))[::-1]
+
+        return res[2] if len(res) >= 3 else res[0]
