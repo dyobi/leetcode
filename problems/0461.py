36ec8066d8fc1a2373d7e15250f62b0f89a983f7
Dyobi <dyobi.92@gmail.com>
2022-01-23 23:27:31 +0900

    .

---
+++ problems/0461.py
@@ -0,0 +1,18 @@
+"461. Hamming Distance"
+
+# The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+# Given two integers x and y, return the Hamming distance between them.
+
+# Input: x = 1, y = 4 / Output: 2
+# Explanation:
+# 1   (0 0 0 1)
+# 4   (0 1 0 0)
+#        ↑   ↑
+# The above arrows point to positions where the corresponding bits are different.
+
+# Input: x = 3, y = 1 / Output: 1
+
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+
+        return bin(x ^ y).count('1')
