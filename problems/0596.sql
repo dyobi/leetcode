429edff31d255378e3fe1360c7dbde1ab0021fa2
Dyobi <dyobi.92@gmail.com>
2022-02-09 23:50:24 +0900

    .

---
+++ problems/0596.sql
@@ -0,0 +1,43 @@
+"596. Classes More Than 5 Students"
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | student     | varchar |
+-- | class       | varchar |
+-- +-------------+---------+
+-- (student, class) is the primary key column for this table.
+-- Each row of this table indicates the name of a student and the class in which they are enrolled.
+
+-- Write an SQL query to report all the classes that have at least five students.
+-- Return the result table in any order.
+
+-- Input: 
+-- Courses table:
+-- +---------+----------+
+-- | student | class    |
+-- +---------+----------+
+-- | A       | Math     |
+-- | B       | English  |
+-- | C       | Math     |
+-- | D       | Biology  |
+-- | E       | Math     |
+-- | F       | Computer |
+-- | G       | Math     |
+-- | H       | Math     |
+-- | I       | Math     |
+-- +---------+----------+
+-- Output: 
+-- +---------+
+-- | class   |
+-- +---------+
+-- | Math    |
+-- +---------+
+-- Explanation: 
+-- - Math has 6 students, so we include it.
+-- - English has 1 student, so we do not include it.
+-- - Biology has 1 student, so we do not include it.
+-- - Computer has 1 student, so we do not include it.
+
+/* Write your PL/SQL query statement below */
+SELECT class FROM courses GROUP BY class HAVING COUNT(*) > 4;
